05859e9e14ddcb4990ae6c0cf95751fe43a6acfa
abhi-j <37021060+abhi-j@users.noreply.github.com>
2021-02-27 22:04:18 +0530

    Added folder
    Made Pages and components folder.
    Made a home page title.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-
+import Home from './Pages/Home/Home';
 
 function App() {
-  return <div>Project Match</div>
-}
+  return (
+    <div>
+    <h1> Project Match </h1>
+    <Home/>
+  </div>
+)}
 
 export default App;
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const Home = () => {
+  return <div>This is the homepage of website </div>
+};
+
+export default Home;
