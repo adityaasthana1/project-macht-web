d31818e882f1a705000292a2a62eddb4fa8c7e6b
neerajsurjaye <neerajsurjaye@gmail.com>
2021-02-28 13:55:51 +0530

    Create home page Template.
    Style the page.

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import About from '../../Components/About/About';
-import Navbar from "../../Components/Navbar/Navbar"
-import Welcome from "../../Components/Welcome/Welcome"
+import Navbar from "../../Components/Navbar/Navbar";
+import Welcome from "../../Components/Welcome/Welcome";
+import PnD from "../../Components/PnD/PnD";
+import VnF from '../../Components/VnF/VnF';
+import Footer from "../../Components/Footer/Footer";
 
 
 // Home page 
@@ -11,6 +14,9 @@ const Home = () => {
     <Navbar></Navbar>
     <Welcome></Welcome>
     <About></About>
+    <PnD></PnD>
+    <VnF></VnF>
+    <Footer></Footer>
   </div>
 };
 
