c4e7483b102bdf45c1f858e2f20a89b8cfc34311
neerajsurjaye <neerajsurjaye@gmail.com>
2021-03-01 16:23:46 +0530

    Ui Basic

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -3,8 +3,9 @@ import "./Welcome.css";
 
 
 let Welcome = () => {
-    return <div>
+    return <div className="Welcome">
         <h1 className="TextMain">Project Macht</h1>
+        <div className="Back"></div>
         {/* Replace with a background image */}
         <img src='#' alt="Hero"></img>
     </div>
