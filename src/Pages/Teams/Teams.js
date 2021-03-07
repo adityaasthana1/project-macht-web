e965672342332c7b8186393805f19bda49865a5b
neerajsurjaye <neerajsurjaye@gmail.com>
2021-03-07 23:55:53 +0530

    FireBase Implemented and added a loader

--- src/Pages/Teams/Teams.js
+++ src/Pages/Teams/Teams.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../Components/Navbar/Navbar"
 import Profile from "../../Components/Profile/Profile"
 import "./profilepage.css"
 // import logo from "../../Img/temp.jpg"
 import profileData from "../../Data/Profiles"
 import fire from "../../Scripts/Fire"
+import { CircularProgress } from "@material-ui/core";
+import { Loader } from "semantic-ui-react";
 
 let generateCards = (users) => {
+    console.log("user", users);
+    if (!users) {
+        return <div class="lds-ripple"><div></div><div></div></div>
+    }
 
     let cardsRow = [];
     let counter = 0;
+
 
     for (let data in users) {
         cardsRow.push(<Profile name={users[data].name} desc={users[data].desc} key={counter}></Profile>);
@@ -21,14 +28,18 @@ let generateCards = (users) => {
 
 let Teams = () => {
     // generateCards(profileData);
+    let [user, setUser] = useState();
 
-    console.log(fire.getUser());
+    useEffect(async () => {
+        setUser(await fire.getUser());
+    }, [])
+
     return <div className="teams-page">
         <Navbar></Navbar>
         <div className="team-header">Meet The Team</div>
         <div className="profile-page">
             <div className="h-profile">
-                {generateCards(profileData)}
+                {generateCards(user)}
                 {/* <Profile name="data" src={logo} info="some lorem data"></Profile> */}
             </div>
         </div>
