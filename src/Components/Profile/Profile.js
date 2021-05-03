a264766b6ed7bed1dca03f70143c076f97d11079
neerajsurjaye <neerajsurjaye@gmail.com>
2021-05-03 15:28:51 +0530

    small commit

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -14,14 +14,22 @@ let Profile = (props) => {
     //         </div>
     //     </div>
 
-    return <figure class="snip1344" onClick={() => {
-        history.push(`/members/${props.id}`);
-    }}>
-        <img src={props.img} alt="profile-sample1" class="background" /><img src={props.img} alt="ERR : No profile " class="profile" />
-        <figcaption>
-            <h3>{props.name}<span>{props.desc}</span></h3>
-        </figcaption>
-    </figure>
+    // return <figure class="snip1344" onClick={() => {
+    //     history.push(`/members/${props.id}`);
+    // }}>
+    //     <img src={props.img} alt="profile-sample1" class="background" /><img src={props.img} alt="ERR : No profile " class="profile" />
+    //     <figcaption>
+    //         <h3>{props.name}<span>{props.desc}</span></h3>
+    //     </figcaption>
+    // </figure>
+
+    console.log("data", props)
+    return <div className="ProfileCont">
+        <img className="ProfilePic" src={props.img} alt="profile-pic"></img>
+        <h3 className="ProfileName">{props.name}</h3>
+        <p className="ProfileDesc">{props.desc}</p>
+        <div className="ProfileViewProfile" onClick={() => { history.push(`/members/${props.id}`) }}>ViewProfile</div>
+    </div>
 
 }
 
